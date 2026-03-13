23d495b03b0103cd487690655c7694fda3dd6798
so-ya <>
2026-03-13 15:58:17 +0900

    Backs.cpp

--- DUYD/Source/Backs.cpp
+++ DUYD/Source/Backs.cpp
@@ -1,42 +1,58 @@
 #include "Backs.h"
 #include "Game.h"
 
-
-
-Backs::Backs(int size)
-{
-    hImage[0] = LoadGraph("data/floor.png");
-    hImage[1] = LoadGraph("data/‰æ‘œ1.png");
-    hImage[2] = LoadGraph("data/wall.png");
-    hImage[3] = LoadGraph("data/stairs.png");
-    hImage[4] = LoadGraph("data/iwa.png.png");
-    hImage[5] = LoadGraph("data/sanso.png");
-    hImage[6] = LoadGraph("data/kyouka.png");
-    size1 = size;
+Backs::Backs(int size) 
+{ 
+	hImage[0] = LoadGraph("data/map/floor.png");
+	hImage[1] = LoadGraph("data/sample/‰æ‘œ1.png");
+	hImage[2] = LoadGraph("data/map/wall.png");
+	hImage[3] = LoadGraph("data/map/stairs.png");
+	hImage[4] = LoadGraph("data/map/iwa.png.png");
+	hImage[5] = LoadGraph("data/sample/sanso.png");
+	hImage[6] = LoadGraph("data/sample/kyouka.png");
 }
 
 Backs::~Backs()
 {
 }
 
-void Backs::Update()
-{
+void Backs::Update() {
+
 }
 
-void Backs::Draw()
-{
-    Game* game = FindGameObject<Game>();
-    if (game == nullptr) return;
-    for (int i = 0; i < game->HEIGHT; i++) {
-        for (int j = 0; j < game->WIDTH; j++) {
-            switch (game->num[i][j]) {
-            case 0: DrawExtendGraph(j * size1, i * size1, (j + 1) * size1, (i + 1) * size1, hImage[1], 0); break;
-            case 1: DrawExtendGraph(j * size1, i * size1, (j + 1) * size1, (i + 1) * size1, hImage[2], 0); break;
-            case 2: DrawExtendGraph(j * size1, i * size1, (j + 1) * size1, (i + 1) * size1, hImage[0], 0); break;
-            case 3: DrawExtendGraph(j * size1, i * size1, (j + 1) * size1, (i + 1) * size1, hImage[0], 0); break;
-            case 4: DrawExtendGraph(j * size1, i * size1, (j + 1) * size1, (i + 1) * size1, hImage[0], 0); break;
-            case 5: DrawExtendGraph(j * size1, i * size1, (j + 1) * size1, (i + 1) * size1, hImage[3], 0); break;
-            }
-        }
-    }
+void Backs::Draw() {
+	Game* game = FindGameObject<Game>();
+	for (int i = 0; i < game->WIDTH; i += 1) {
+		for (int j = 0; j < game->HEIGHT; j += 1) {
+			switch (game->num[i][j]) {
+			case 0:
+				DrawExtendGraph(i * size1, j * size1, (i * size1) + size1, (j * size1) + size1, hImage[1], 0);
+				break;
+
+			case 1:
+				DrawExtendGraph(i * size1, j * size1, (i * size1) + size1, (j * size1) + size1, hImage[2], 0);
+				break;
+
+			case 2:
+				DrawExtendGraph(i * size1, j * size1, (i * size1) + size1, (j * size1) + size1, hImage[0], 0);
+				break;
+
+			case 3:
+				DrawExtendGraph(i * size1, j * size1, (i * size1) + size1, (j * size1) + size1, hImage[0], 0);
+				break;
+
+			case 4:
+				DrawExtendGraph(i * size1, j * size1, (i * size1) + size1, (j * size1) + size1, hImage[0], 0);
+				break;
+
+			case 5:
+				DrawExtendGraph(i * size1, j * size1, (i * size1) + size1, (j * size1) + size1, hImage[3], 0);
+				break;
+
+			case 6:
+				DrawExtendGraph(i * size1, j * size1, (i * size1) + size1, (j * size1) + size1, hImage[0], 0);
+				break;
+			}
+		}
+	}
 }
