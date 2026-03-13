608c60b4e3e1f78e78fabe66d375c1859bf260d9
so-ya <>
2026-03-13 16:05:00 +0900

    Game

--- DUYD/Source/Game.cpp
+++ DUYD/Source/Game.cpp
@@ -3,7 +3,10 @@
 #include "Rocks.h"
 #include "GlobalStatus.h"
 #include "Enemy.h"
+#include "GamePlayer.h"
+#include "status.h"
 #include <random>
+#include <cstring>
 
 
 Game::Game()
@@ -51,36 +54,31 @@ Game::Game()
 	}
  new Backs(size);
 
-	for (int i = 0; i < HEIGHT; i += 1) {
-		for (int j = 0; j < WIDTH; j += 1) {
-			if (num[i][j] == 2 || num[i][j] == 5) {
-				if (Random(0, 2) > 0) {
-					new Rocks(i * size, j * size, size);
-					isRocks = true;
-				}
-				if (Random(0, 5) == 0 && !isRocks) {
-					new Enemy(i * size, j * size, size);
-				}
-			}
-			if (num[i][j] == 3 || num[i][j] == 4) {
-				new Rocks(i * size, j * size, size);
-			}
-			isRocks = false;
-		}
-	}
-	int startX = 64, startY = 64;
-	for (int i = 0; i < HEIGHT; i++) {
-		for (int j = 0; j < WIDTH; j++) {
-			if (num[i][j] == 1) {
-				startX = j * TILE_SIZE;
-				startY = i * TILE_SIZE;
-				goto found;
-			}
-		}
-	}
-found:
-	player = new Player(this, startX, startY);
-	player->SetStatusReference(&GlobalStatus::Get());
+ for (int i = 0; i < WIDTH; i += 1) {
+	 for (int j = 0; j < HEIGHT; j += 1) {
+		 if (num[i][j] == 2) {
+			 if (Random(0, 2) > 0) {
+				 new Rocks(i * size, j * size, size);
+				 isRocks = true;
+				 num[i][j] == 6;
+			 }
+			 if (Random(0, 5) == 0 && !isRocks) {
+				 new Enemy(i * size, j * size, size);
+				 isEnemy = true;
+			 }
+			 if (!isRocks && !isEnemy && !isPlayer) {
+				 isPlayer = true;
+				 new GamePlayer(i * size, j * size, size);
+			 }
+		 }
+		 if (num[i][j] == 3 || num[i][j] == 4) {
+			 new Rocks(i * size, j * size, size);
+		 }
+		 isRocks = false;
+		 isEnemy = false;
+	 }
+ }
+
 }
 
 Game::~Game()
@@ -88,17 +86,7 @@ Game::~Game()
 }
 
 void Game::Update() {
-	
-	GlobalStatus::Get().Update();
-	cameraX = player->GetX() - 1920 / 2 + 16;
-	cameraY = player->GetY() - 1080 / 2 + 16;
 
-	int maxCameraX = WIDTH * TILE_SIZE - 1920;
-	int maxCameraY = HEIGHT * TILE_SIZE - 1080;
-	if (cameraX < 0) cameraX = 0;
-	if (cameraY < 0) cameraY = 0;
-	if (cameraX > maxCameraX) cameraX = maxCameraX;
-	if (cameraY > maxCameraY) cameraY = maxCameraY;
 }
 
 void Game::Draw() {
@@ -106,51 +94,14 @@ void Game::Draw() {
 	GlobalStatus::Get().Draw();
 }
 
-bool Game::IsWall(int tileX, int tileY)
+bool Game::isWall(int tileX, int tileY)
 {
 	if (tileX < 0 || tileX >= WIDTH || tileY < 0 || tileY >= HEIGHT)
 		return true;
-	return num[tileY][tileX] == 0;
+
+	int tile = tilegame[tileX][tileY]; // [x][y] ÇÃèáî‘ÅI
+	return tile == WALL_A || tile == WALL_B;
 }
-
-bool Game::CanMove(int pixelX, int pixelY)
-{
-	const int playerSize = 32;
-	int left = pixelX / TILE_SIZE;
-	int right = (pixelX + playerSize - 1) / TILE_SIZE;
-	int top = pixelY / TILE_SIZE;
-	int bottom = (pixelY + playerSize - 1) / TILE_SIZE;
-
-	return !IsWall(left, top) && !IsWall(right, top) &&
-		!IsWall(left, bottom) && !IsWall(right, bottom);
-}
-
-int Game::GetTileType(int pixelX, int pixelY)
-{
-	int tileX = pixelX / TILE_SIZE;
-	int tileY = pixelY / TILE_SIZE;
-	if (tileX < 0 || tileX >= WIDTH || tileY < 0 || tileY >= HEIGHT)
-		return -1;
-	return num[tileY][tileX];
-}
-
-void Game::DigTile(int pixelX, int pixelY)
-{
-	int tileX = pixelX / TILE_SIZE;
-	int tileY = pixelY / TILE_SIZE;
-	if (tileX < 0 || tileX >= WIDTH || tileY < 0 || tileY >= HEIGHT)
-		return;
-	if (num[tileY][tileX] == 2 || num[tileY][tileX] == 3 || num[tileY][tileX] == 4)
-		num[tileY][tileX] = 1;
-}
-
-
-
-
-
-
-
-
 
 int Game::Random(int min, int max) {
 	std::random_device rd;
