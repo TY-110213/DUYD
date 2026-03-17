8f4cd45026c50f43648ab1a332376ed9bbb37dba
TY-110213 <setsuna110213@gmail.com>
2026-03-17 14:38:02 +0900

    素材の変更及び修正

--- DUYD/Source/Tansakunosyo.cpp
+++ DUYD/Source/Tansakunosyo.cpp
@@ -1,8 +1,12 @@
 #include "Tansakunosyo.h"
+#include <DxLib.h>
+#include "Screen.h"
+
+int TansakuImage;
 
 Tansakunosyo::Tansakunosyo()
 {
-	
+	TansakuImage = LoadGraph("data/screen/’Tõ‚Ì‘.png");
 }
 
 Tansakunosyo::~Tansakunosyo()
@@ -15,6 +19,6 @@ void Tansakunosyo::Update()
 
 void Tansakunosyo::Draw()
 {
-	DrawString(0, 0, "’Tõ‚Ì‘", GetColor(255, 255, 255));
-
+	//DrawString(0, 0, "’Tõ‚Ì‘", GetColor(255, 255, 255));
+	DrawExtendGraph(0, 0, 1920, 1080, TansakuImage, TRUE);//¶ã(0,0)‚©‚ç‰E‰º(1920,1080)‚Ì”ÍˆÍ‚É‰æ‘œ‚ğˆø‚«L‚Î‚µ‚Ä•\¦‚·‚é@•¶š‚¢‚ç‚ê‚ñ
 }
