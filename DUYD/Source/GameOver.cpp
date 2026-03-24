c1da59e71b19ba3d6b4c1efa69a2af7b19aa58af
so-ya <>
2026-03-24 15:13:04 +0900

    GameOver.cpp

---
+++ DUYD/Source/GameOver.cpp
@@ -0,0 +1,32 @@
+#include "GameOver.h"
+#include "screen.h"
+#include "Game.h"
+
+GameOver::GameOver()
+{
+	hImage = LoadGraph("data/screen/gameover.png");
+}
+
+GameOver::~GameOver()
+{
+
+}
+
+void GameOver::Update()
+{
+}
+
+void GameOver::Draw()
+{
+	Game* game = FindGameObject<Game>();
+	
+	DrawExtendGraph(0, 0, 1920, 1080, hImage, true);
+	SetFontSize(55);
+	if (game->area < 100) {
+		DrawFormatString(690, 710, GetColor(0, 0, 0), "%01d", game->area);
+	}
+	else {
+		//3Œ…ˆÈã‚Ì‚Ì•\¦ˆÊ’uC³—p
+	}
+	SetFontSize(15);
+}
